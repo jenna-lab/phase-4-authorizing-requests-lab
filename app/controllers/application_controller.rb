51c3a0f74d24f1bce4daaa2a745199eff46a9846
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-03-22 19:55:33 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  def user_not_authenticated
+    render json: { error: 'You need to sign in or sign up
 
 end
